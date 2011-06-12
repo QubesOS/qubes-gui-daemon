c238b5b6a8e705d34645da92def790974058f4dd
Marek Marczykowski <marmarek@mimuw.edu.pl>
2011-06-12 02:40:48 +0200

    vm: parse \n \t etc in qubes_keyboard

--- appvm_scripts/etc/X11/xinit/xinitrc.d/qubes_keymap.sh
+++ appvm_scripts/etc/X11/xinit/xinitrc.d/qubes_keymap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 QUBES_KEYMAP="`/usr/bin/xenstore-read qubes_keyboard`"
+QUBES_KEYMAP="`echo -e $QUBES_KEYMAP`"
 
 if [ -n "$QUBES_KEYMAP" ]; then
     echo "$QUBES_KEYMAP" | xkbcomp - $DISPLAY
