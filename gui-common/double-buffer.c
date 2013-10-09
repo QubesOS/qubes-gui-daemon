c441de9a88c4107726412fdcb03b9b9d8f3d9cba
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2013-10-09 17:23:38 -0400

    double-buffer: use fully qualified for xmessage
    Avoids the worry of calling anything else.

--- gui-common/double-buffer.c
+++ gui-common/double-buffer.c
@@ -55,7 +55,7 @@ void double_buffer_append(char *buf, int size)
 				"double_buffer_append: offset=%d, data_count=%d, req_size=%d\n",
 				data_offset, data_count, size);
 			system
-			    ("xmessage -button OK:2 'Out of buffer space (AppVM refuses to read data?), terminating...'");
+			    ("/usr/bin/xmessage -button OK:2 'Out of buffer space (AppVM refuses to read data?), terminating...'");
 			exit(1);
 		}
 		newbuf = malloc(newsize);
