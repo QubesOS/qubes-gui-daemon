QubesOS/qubes-gui-daemon

commit 157ca1496ae0a6e578c124eb0c57217268d3ea3d

Unset override-redirect flag when docking, instead of preventing dock
Apparently some applications do try to dock override-redirect window.
While in theory it is harmless (override-redirect flag applies to top
level windows only), QSB-072 shows that both window manager
and gui-daemon can be easily confused about who should manage such
window, when combined with other features/states. For this reason, unset
the override redirect flag before docking the window. This avoids having
unexpected override-redirect toplevel window if the panel (dock) is ever
terminated.

gui-daemon/xside.c
@@ -3055,9 +3055,15 @@ static void handle_dock(Ghandles * g, struct windowdata *vm_window)
         return;
     }
     if (vm_window->override_redirect) {
-        fprintf(stderr, "cannot dock override-redirect window 0x%x\n",
+        XSetWindowAttributes attr;
+        fprintf(stderr, "docking an override-redirect window 0x%x - "
+                "clearing override-redirect\n",
                 (int) vm_window->local_winid);
-        return;
+        /* changing directly is safe, because window is not mapped here yet */
+        attr.override_redirect = vm_window->override_redirect;
+        XChangeWindowAttributes(g->display, vm_window->local_winid,
+                CWOverrideRedirect, &attr);
+        vm_window->override_redirect = 0;
     }
     if (vm_window->parent) {
         fprintf(stderr, "cannot dock non-top level window 0x%x\n",

commit a91639e15d616f2ec4e603c088fe60e774134877

Actually unset override-redirect when docking
This was supposed to be done in 157ca1496ae0a6e578c124eb0c57217268d3ea3d,
but the implementation was wrong: it set the override-redirect flag to
what the GUI daemon thought the current value was, not 0.  Therefore,
the GUI daemon could become confused as to the override-redirect state
of a window.

This turns out not to be exploitable in practice unless subwindows are
enabled.  With subwindows disabled, it is not possible to prevent the
window from being marked as docked, which prevents moving, resizing, or
mapping the window.  Since the window cannot be mapped by the VM, it
cannot appear on screen unless the embedder tells the GUI daemon to map
it.  The embedder will resize the window before sending this message, so
there is no window of vulnerability.

Furthermore, subwindows are disabled in the default configuration.
Because QubesOS/qubes-core-admin-client#221 has not been merged, it is
not possible to turn them on with qvm-features.  Turning them on
manually (by editing qvm-start-daemon or similar) is not supported.

Fixes: 157ca1496ae0 ("Unset override-redirect flag when docking, instead of preventing dock")

gui-daemon/xside.c
@@ -3105,10 +3105,9 @@ static void handle_dock(Ghandles * g, struct windowdata *vm_window)
                 "clearing override-redirect\n",
                 (int) vm_window->local_winid);
         /* changing directly is safe, because window is not mapped here yet */
-        attr.override_redirect = vm_window->override_redirect;
+        attr.override_redirect = vm_window->override_redirect = 0;
         XChangeWindowAttributes(g->display, vm_window->local_winid,
                 CWOverrideRedirect, &attr);
-        vm_window->override_redirect = 0;
     }
     if (vm_window->parent) {
         fprintf(stderr, "cannot dock non-top level window 0x%x\n",