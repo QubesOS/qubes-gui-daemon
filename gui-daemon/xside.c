3a74c63dd4479576cb90ccd544e14999aa52e468
Aaron Rainbolt <arraybolt3@ubuntu.com>
2024-10-28 14:30:50 -0400

    Replace magic quotes with normal quotes in parse_vm_config

--- gui-daemon/xside.c
+++ gui-daemon/xside.c
@@ -4477,7 +4477,7 @@ static void parse_vm_config(Ghandles * g, config_setting_t * group)
             g->disable_override_redirect = 0;
         else {
             fprintf(stderr,
-                    "unsupported value ‘%s’ for override_redirect (must be ‘disabled’ or ‘allow’)\n",
+                    "unsupported value '%s' for override_redirect (must be 'disabled' or 'allow')\n",
                     value);
             exit(1);
         }
